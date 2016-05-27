6cbc3d7fcb148d3a0eda422bd3ea9204450ede8e
Hong Le <honglv@dfm-engineering.com>
2016-05-27 16:53:08 +0700

    Initialize project compare image in .NET

---
+++ CompareImage.NET/frmCompare.Designer.vb
@@ -0,0 +1,193 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCompare
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.panel = New System.Windows.Forms.Panel()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnCompare = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnImageDes = New System.Windows.Forms.Button()
+        Me.btnImageSrc = New System.Windows.Forms.Button()
+        Me.destinationLabel = New System.Windows.Forms.Label()
+        Me.destinationPictureBox = New System.Windows.Forms.PictureBox()
+        Me.sourceLabel = New System.Windows.Forms.Label()
+        Me.sourcePictureBox = New System.Windows.Forms.PictureBox()
+        Me.panel.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.destinationPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.sourcePictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'panel
+        '
+        Me.panel.Controls.Add(Me.GroupBox1)
+        Me.panel.Controls.Add(Me.btnImageDes)
+        Me.panel.Controls.Add(Me.btnImageSrc)
+        Me.panel.Controls.Add(Me.destinationLabel)
+        Me.panel.Controls.Add(Me.destinationPictureBox)
+        Me.panel.Controls.Add(Me.sourceLabel)
+        Me.panel.Controls.Add(Me.sourcePictureBox)
+        Me.panel.Location = New System.Drawing.Point(12, 9)
+        Me.panel.Name = "panel"
+        Me.panel.Size = New System.Drawing.Size(517, 335)
+        Me.panel.TabIndex = 16
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnCompare)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Location = New System.Drawing.Point(9, 268)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(492, 58)
+        Me.GroupBox1.TabIndex = 22
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Result"
+        '
+        'btnCompare
+        '
+        Me.btnCompare.Location = New System.Drawing.Point(6, 21)
+        Me.btnCompare.Name = "btnCompare"
+        Me.btnCompare.Size = New System.Drawing.Size(75, 23)
+        Me.btnCompare.TabIndex = 16
+        Me.btnCompare.Text = "Comapre"
+        Me.btnCompare.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(192, 29)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(13, 13)
+        Me.Label3.TabIndex = 21
+        Me.Label3.Text = "0"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(106, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(79, 13)
+        Me.Label1.TabIndex = 17
+        Me.Label1.Text = "Difference (%): "
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(313, 26)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(125, 13)
+        Me.Label2.TabIndex = 20
+        Me.Label2.Text = "The images are identical."
+        '
+        'btnImageDes
+        '
+        Me.btnImageDes.Location = New System.Drawing.Point(325, 225)
+        Me.btnImageDes.Name = "btnImageDes"
+        Me.btnImageDes.Size = New System.Drawing.Size(75, 23)
+        Me.btnImageDes.TabIndex = 19
+        Me.btnImageDes.Text = "Image 2"
+        Me.btnImageDes.UseVisualStyleBackColor = True
+        '
+        'btnImageSrc
+        '
+        Me.btnImageSrc.Location = New System.Drawing.Point(95, 226)
+        Me.btnImageSrc.Name = "btnImageSrc"
+        Me.btnImageSrc.Size = New System.Drawing.Size(75, 23)
+        Me.btnImageSrc.TabIndex = 18
+        Me.btnImageSrc.Text = "Image 1"
+        Me.btnImageSrc.UseVisualStyleBackColor = True
+        '
+        'destinationLabel
+        '
+        Me.destinationLabel.AutoSize = True
+        Me.destinationLabel.Location = New System.Drawing.Point(270, 4)
+        Me.destinationLabel.Name = "destinationLabel"
+        Me.destinationLabel.Size = New System.Drawing.Size(60, 13)
+        Me.destinationLabel.TabIndex = 15
+        Me.destinationLabel.Text = "Destination"
+        '
+        'destinationPictureBox
+        '
+        Me.destinationPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.destinationPictureBox.Location = New System.Drawing.Point(267, 20)
+        Me.destinationPictureBox.Name = "destinationPictureBox"
+        Me.destinationPictureBox.Size = New System.Drawing.Size(180, 199)
+        Me.destinationPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.destinationPictureBox.TabIndex = 14
+        Me.destinationPictureBox.TabStop = False
+        '
+        'sourceLabel
+        '
+        Me.sourceLabel.AutoSize = True
+        Me.sourceLabel.Location = New System.Drawing.Point(59, 4)
+        Me.sourceLabel.Name = "sourceLabel"
+        Me.sourceLabel.Size = New System.Drawing.Size(41, 13)
+        Me.sourceLabel.TabIndex = 13
+        Me.sourceLabel.Text = "Source"
+        '
+        'sourcePictureBox
+        '
+        Me.sourcePictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.sourcePictureBox.Location = New System.Drawing.Point(56, 20)
+        Me.sourcePictureBox.Name = "sourcePictureBox"
+        Me.sourcePictureBox.Size = New System.Drawing.Size(180, 199)
+        Me.sourcePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.sourcePictureBox.TabIndex = 12
+        Me.sourcePictureBox.TabStop = False
+        '
+        'frmCompare
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(535, 351)
+        Me.Controls.Add(Me.panel)
+        Me.Name = "frmCompare"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Compare Image in .NET"
+        Me.panel.ResumeLayout(False)
+        Me.panel.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.destinationPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.sourcePictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Private WithEvents panel As System.Windows.Forms.Panel
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents btnImageDes As System.Windows.Forms.Button
+    Friend WithEvents btnImageSrc As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btnCompare As System.Windows.Forms.Button
+    Private WithEvents destinationLabel As System.Windows.Forms.Label
+    Private WithEvents destinationPictureBox As System.Windows.Forms.PictureBox
+    Private WithEvents sourceLabel As System.Windows.Forms.Label
+    Private WithEvents sourcePictureBox As System.Windows.Forms.PictureBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+
+End Class
---
+++ CompareImage.NET/frmCompare.vb
@@ -0,0 +1,156 @@
+﻿Imports System.IO
+Imports System.Drawing.Drawing2D
+Imports System.Drawing.Imaging
+
+Public Class frmCompare
+    Private ReadOnly matrix As ColorMatrix = New ColorMatrix(New Single()() { _
+                                                             New Single() {0.3F, 0.3F, 0.3F, 0, 0}, _
+                                                             New Single() {0.59F, 0.59F, 0.59F, 0, 0}, _
+                                                             New Single() {0.11F, 0.11F, 0.11F, 0, 0}, _
+                                                             New Single() {0, 0, 0, 1, 0}, _
+                                                             New Single() {0, 0, 0, 0, 1}})
+
+    Dim path(1) As String
+
+    Private Sub btnImageSrc_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnImageSrc.Click
+        Dim openDialogOpen As New OpenFileDialog
+        'Set the Open dialog properties
+        With openDialogOpen
+            .Filter = "JPEG (*.jpg)|*.jpg"
+            .FilterIndex = 1
+            .Title = "Open File Dialog"
+        End With
+
+        If openDialogOpen.ShowDialog = Windows.Forms.DialogResult.OK Then
+            'Read the contents of the file
+            Dim NomFic As String = openDialogOpen.FileName
+            path(0) = NomFic
+            sourcePictureBox.Load(NomFic)
+        End If
+    End Sub
+
+    Private Sub btnImageDes_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnImageDes.Click
+        Dim openDialogOpen As New OpenFileDialog
+        'Set the Open dialog properties
+        With openDialogOpen
+            .Filter = "JPEG (*.jpg)|*.jpg"
+            .FilterIndex = 1
+            .Title = "Open File Dialog"
+        End With
+
+        If openDialogOpen.ShowDialog = Windows.Forms.DialogResult.OK Then
+            'Read the contents of the file
+            Dim NomFic As String = openDialogOpen.FileName
+            path(1) = NomFic
+            destinationPictureBox.Load(NomFic)
+        End If
+    End Sub
+
+    Private Function ResizeImage(ByVal originalImage As Image, ByVal newWidth As Integer, ByVal newHeight As Integer) As Image
+        Dim smallVersion As Image = New Bitmap(newWidth, newHeight)
+        Using g As Graphics = Graphics.FromImage(smallVersion)
+            g.SmoothingMode = SmoothingMode.HighQuality
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic
+            g.PixelOffsetMode = PixelOffsetMode.HighQuality
+            g.DrawImage(originalImage, 0, 0, newWidth, newHeight)
+        End Using
+
+        Return smallVersion
+    End Function
+
+    Private Function GetGrayScaleVersion(ByVal original As Image) As Image
+        'create a blank bitmap the same size as original
+        Dim newBitmap As Bitmap = New Bitmap(original.Width, original.Height)
+
+        'get a graphics object from the new image
+        Using g As Graphics = Graphics.FromImage(newBitmap)
+            'create some image attributes
+            Dim attributes As ImageAttributes = New ImageAttributes()
+
+            'set the color matrix attribute
+            attributes.SetColorMatrix(matrix)
+
+            'draw the original image on the new image
+            'using the grayscale color matrix
+            g.DrawImage(original, New Rectangle(0, 0, original.Width, original.Height),
+               0, 0, original.Width, original.Height, GraphicsUnit.Pixel, attributes)
+        End Using
+        Return newBitmap
+    End Function
+
+    Private Function GetGrayScaleValues(ByVal img As Image) As Double(,)
+        Using thisOne As Bitmap = CType(GetGrayScaleVersion(ResizeImage(img, 16, 16)), Bitmap)
+            Dim grayScale(15, 15) As Double
+
+            For y As Integer = 0 To 15
+                For x As Integer = 0 To 15
+                    grayScale(x, y) = Math.Abs(thisOne.GetPixel(x, y).R)
+                Next
+            Next
+            Return grayScale
+        End Using
+
+    End Function
+
+    Private Function GetDifferences(ByVal img1 As Image, ByVal img2 As Image) As Double(,)
+        Dim thisOne As Bitmap = CType(GetGrayScaleVersion(ResizeImage(img1, 16, 16)), Bitmap)
+        Dim theOtherOne As Bitmap = CType(GetGrayScaleVersion(ResizeImage(img2, 16, 16)), Bitmap)
+
+        Dim differences(15, 15) As Double
+        Dim firstGray(15, 15) As Double
+        Dim secondGray(15, 15) As Double
+
+        firstGray = GetGrayScaleValues(thisOne)
+        secondGray = GetGrayScaleValues(theOtherOne)
+
+        For y As Integer = 0 To 15
+            For x As Integer = 0 To 15
+                differences(x, y) = Math.Abs(firstGray(x, y) - secondGray(x, y))
+            Next
+        Next
+
+        thisOne.Dispose()
+        theOtherOne.Dispose()
+
+        Return differences
+    End Function
+
+    Private Function PercentageDifference(ByVal img1 As Image, ByVal img2 As Image, Optional ByVal threshold As Double = 5) As Double
+        Dim differences(15, 15) As Double
+        differences = GetDifferences(img1, img2)
+
+        Dim diffPixels As Integer = 0
+
+        For Each b As Double In differences
+            If (b > threshold) Then
+                diffPixels = diffPixels + 1
+            End If
+        Next
+
+        Return diffPixels / 256.0F
+    End Function
+
+    Private Function CheckIfFileExists(ByVal filePath As String) As Boolean
+        If File.Exists(filePath) = False Then
+            Throw New FileNotFoundException("File '" + filePath + "' not found!")
+        End If
+        Return True
+    End Function
+
+    Private Sub btnCompare_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCompare.Click
+        ' Load the images.
+        Dim bm1 As Bitmap = Image.FromFile(path(0))
+        Dim bm2 As Bitmap = Image.FromFile(path(1))
+
+        Dim diff As Double = PercentageDifference(bm1, bm2)
+        Label3.Text = (diff * 100).ToString
+        If diff < 0.1 Then
+            Label2.Text = "The images are identical."
+        Else
+            Label2.Text = "The images are different."
+        End If
+
+        bm1.Dispose()
+        bm2.Dispose()
+    End Sub
+End Class
